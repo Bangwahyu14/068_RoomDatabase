c8041e931bf5982e732b1ce762e9dd3ab3b63d93
Wahyu Agung Prasetyo <wahyu.agung.ft23@mail.umy.ac.id>
2025-12-04 12:02:53 +0700

    menambahkan  initializer detail view model

--- app/src/main/java/com/example/roomdatabase/viewmodel/Profider/penyediaviewmodel.kt
+++ app/src/main/java/com/example/roomdatabase/viewmodel/Profider/penyediaviewmodel.kt
@@ -2,10 +2,12 @@ package com.example.roomdatabase.viewmodel.Profider
 
 import EntryViewModel
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.createSavedStateHandle
 import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
 import com.example.roomdatabase.repositori.AplikasiSiswa
+import com.example.roomdatabase.viewmodel.DetailViewModel
 import com.example.roomdatabase.viewmodel.HomeViewModel
 
 object PenyediaViewModel{
@@ -15,6 +17,9 @@ object PenyediaViewModel{
         }
         initializer {
             EntryViewModel(aplikasiSiswa().container.repositoriSiswa)
+        }
+        initializer {
+            DetailViewModel(this.createSavedStateHandle(),aplikasiSiswa().container.repositoriSiswa)
         }
     }
 }
