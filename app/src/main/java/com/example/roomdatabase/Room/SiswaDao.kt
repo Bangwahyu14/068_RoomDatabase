d1ddeeea80969f8f8bee9525324041cb389cfd08
Wahyu Agung Prasetyo <wahyu.agung.ft23@mail.umy.ac.id>
2025-12-04 11:34:46 +0700

    menambahkan  delete pada siswa dao

--- app/src/main/java/com/example/roomdatabase/Room/SiswaDao.kt
+++ app/src/main/java/com/example/roomdatabase/Room/SiswaDao.kt
@@ -5,6 +5,7 @@ import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
+import androidx.room.Update
 import kotlinx.coroutines.flow.Flow
 
 @Dao
@@ -19,5 +20,8 @@ interface SiswaDao {
     //edit
     @Query("SELECT * from tblSiswa WHERE id = :id")
     fun getSiswa(id: Int): Flow<Siswa>
+
+    @Delete
+    suspend fun delete(siswa: Siswa)
 
 }
